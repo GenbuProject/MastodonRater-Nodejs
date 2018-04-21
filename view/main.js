bbe8d99961bd9775a44ec2976a3ff375c4616332
Genbu Hase <hasegenbu@gmail.com>
2018-04-22 08:53:53 +0900

    Add a field to each elements, "Data-I18n"

--- view/main.js
+++ view/main.js
@@ -1,6 +1,16 @@
 const SITEURL = location.href.replace(location.search, "");
 
+const privacyPicker = document.getElementById("main-controlPanel-privacy");
+
 window.addEventListener("DOMContentLoaded", () => {
 	document.querySelectorAll("Select").forEach(select => M.FormSelect.init(select));
 	document.querySelectorAll(".dropdown-target").forEach(dropdownTarget => M.Dropdown.init(dropdownTarget));
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+	privacyPicker.addEventListener("change", event => {
+		let privacy = event.target.value;
+
+		//localStorage.setItem("com.GenbuProject.MastodonRater.currentPrivacy", privacy);
+	});
 });
